14f63f2edb8e04daa6696d47a681c2363aa70463
Kristian Mandrup <kmandrup@gmail.com>
2011-11-19 22:09:39 +0100

    major refactor

---
+++ lib/cantango/configuration/permit_store.rb
@@ -0,0 +1,20 @@
+module CanTango
+  class Configuration
+    class PermitStore
+      include CanTango::Configuration::Factory
+
+      def type_options
+        {:type => default_type}
+      end
+
+      def default_type= type
+        raise ArgumentError, "Must be a String or Symbol, was #{type}" if !type.kind_of_label?
+        @default_type = type.to_sym
+      end
+
+      def default_type
+        @default_type || :memory
+      end
+    end
+  end
+end
