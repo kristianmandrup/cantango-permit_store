aa9974b8481de181048aaa6691e233922de96b69
Kristian Mandrup <kmandrup@gmail.com>
2011-12-25 22:28:38 +0100

    statement specs

--- spec/cantango/permit_store/execute/statement/single_spec.rb
+++ spec/cantango/permit_store/execute/statement/single_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe CanTango::PermitStore::Execute::Statement::Single do
-  pending
+  before do
+    @method = :can
+    @actions = :edit
+    @conditions = 'Project'
+    @statement = CanTango::PermitStore::Execute::Statement.new @method, @actions, @conditions
+  end
+  
+  describe 'init' do
+    subject { @statement }
+    
+    its(:method)      { should == @method }
+    its(:actions)     { should == @actions }
+    its(:conditions)  { should == @conditions }
+  end
+
+  context 'no conditions' do
+    describe '#to_code' do
+      subject { @statement }    
+      before do
+        @statement.conditions = nil
+      end
+      
+      it 'should not generate full statement' do
+        @statement.to_code.should_not match /:.+,.+/
+      end
+
+      it 'should generate simple statement' do
+        @statement.to_code.should match /:.+/
+      end
+    end
+  end
+
+  context 'with conditions' do
+    describe '#to_code' do
+      subject { @statement }    
+      before do
+        @statement.conditions = @conditions
+      end
+      
+      it 'should generate full statement' do
+        @statement.to_code.should match /:.+,.+/
+      end
+    end
+  end
 end
