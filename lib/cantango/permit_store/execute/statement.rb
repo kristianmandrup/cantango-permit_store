a62f0c3945661c7dd0f41e75c034b3f698286ea8
Kristian Mandrup <kmandrup@gmail.com>
2011-12-25 19:11:57 +0100

    statement works

--- lib/cantango/permit_store/execute/statement.rb
+++ lib/cantango/permit_store/execute/statement.rb
@@ -1,13 +1,21 @@
 module CanTango::PermitStore::Execute
   class Statement
-    attr_reader :method, :actions, :conditions
+    attr_accessor :method, :actions, :conditions
 
     def initialize method, actions, conditions = {}
       @method, @actions, @conditions = [method, actions, conditions]
     end
 
     def to_code
-      line = conditions.empty? ? "#{method}(:#{actions})" : "#{method}(:#{actions}, #{conditions})"
+      conditions.empty? ? simple_statement : full_statement
+    end
+    
+    def simple_statement
+      "#{method}(:#{actions})"
+    end
+
+    def full_statement
+      "#{method}(:#{actions}, #{conditions})"
     end
   end
 end
--- spec/cantango/permit_store/execute/statement_spec.rb
+++ spec/cantango/permit_store/execute/statement_spec.rb
@@ -8,12 +8,42 @@ describe CanTango::PermitStore::Execute::Statement do
     @conditions = 'Project'
     @statement = CanTango::PermitStore::Execute::Statement.new @method, @actions, @conditions
   end
-
-  subject { @statement }
   
   describe 'init' do
+    subject { @statement }
+    
     its(:method)      { should == @method }
     its(:actions)     { should == @actions }
     its(:conditions)  { should == @conditions }
+  end
+
+  context 'no conditions' do
+    describe '#to_code' do
+      subject { @statement }    
+      before do
+        @statement.conditions = nil
+      end
+      
+      it 'should not generate full statement' do
+        @statement.to_code.should_not match /:.+,.+/
+      end
+
+      it 'should generate simple statement' do
+        @statement.to_code.should match /:.+/
+      end
+    end
+  end
+
+  context 'with conditions' do
+    describe '#to_code' do
+      subject { @statement }    
+      before do
+        @statement.conditions = @conditions
+      end
+      
+      it 'should generate full statement' do
+        @statement.to_code.should match /:.+,.+/
+      end
+    end
   end
 end
